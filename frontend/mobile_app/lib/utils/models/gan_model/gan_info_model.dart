6e6e21f2c57eb2278e7853342ef5a91cd4e348a1
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2021-12-27 11:27:20 +0700

    feat(mobile): add gan model

---
+++ frontend/mobile_app/lib/utils/models/gan_model/gan_info_model.dart
@@ -0,0 +1,152 @@
+class GanInfoModel {
+  Null msg;
+  Null error;
+  Data data;
+
+  GanInfoModel({this.msg, this.error, this.data});
+
+  GanInfoModel.fromJson(Map<String, dynamic> json) {
+    msg = json['msg'];
+    error = json['error'];
+    data = json['data'] != null ? new Data.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['msg'] = this.msg;
+    data['error'] = this.error;
+    if (this.data != null) {
+      data['data'] = this.data.toJson();
+    }
+    return data;
+  }
+}
+
+class Data {
+  String id;
+  String imageName;
+  ImageAssets imageAssets;
+  String timestamp;
+
+  Data({this.id, this.imageName, this.imageAssets, this.timestamp});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    imageName = json['image_name'];
+    imageAssets = json['image_assets'] != null
+        ? new ImageAssets.fromJson(json['image_assets'])
+        : null;
+    timestamp = json['timestamp'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['image_name'] = this.imageName;
+    if (this.imageAssets != null) {
+      data['image_assets'] = this.imageAssets.toJson();
+    }
+    data['timestamp'] = this.timestamp;
+    return data;
+  }
+}
+
+class ImageAssets {
+  Originals originals;
+  Enhanced enhanced;
+
+  ImageAssets({this.originals, this.enhanced});
+
+  ImageAssets.fromJson(Map<String, dynamic> json) {
+    originals = json['originals'] != null
+        ? new Originals.fromJson(json['originals'])
+        : null;
+    enhanced = json['enhanced'] != null
+        ? new Enhanced.fromJson(json['enhanced'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.originals != null) {
+      data['originals'] = this.originals.toJson();
+    }
+    if (this.enhanced != null) {
+      data['enhanced'] = this.enhanced.toJson();
+    }
+    return data;
+  }
+}
+
+class Originals {
+  String s128;
+  String s640;
+  String s1080;
+
+  Originals({this.s128, this.s640, this.s1080});
+
+  Originals.fromJson(Map<String, dynamic> json) {
+    s128 = json['128'];
+    s640 = json['640'];
+    s1080 = json['1080'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['128'] = this.s128;
+    data['640'] = this.s640;
+    data['1080'] = this.s1080;
+    return data;
+  }
+}
+
+class Faces {
+  String s128;
+  String s640;
+  String s1080;
+
+  Faces({this.s128, this.s640, this.s1080});
+
+  Faces.fromJson(Map<String, dynamic> json) {
+    s128 = json['128'];
+    s640 = json['640'];
+    s1080 = json['1080'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['128'] = this.s128;
+    data['640'] = this.s640;
+    data['1080'] = this.s1080;
+    return data;
+  }
+}
+
+class Enhanced {
+  Originals full;
+  List<Faces> faces;
+
+  Enhanced({this.full, this.faces});
+
+  Enhanced.fromJson(Map<String, dynamic> json) {
+    full = json['full'] != null ? new Originals.fromJson(json['full']) : null;
+    if (json['faces'] != null) {
+      // ignore: deprecated_member_use
+      faces = new List<Faces>();
+      json['faces'].forEach((v) {
+        faces.add(new Faces.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.full != null) {
+      data['full'] = this.full.toJson();
+    }
+    if (this.faces != null) {
+      data['faces'] = this.faces.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
